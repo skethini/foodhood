fd54725646bc8a8c4745f671dd1b7629919ff110
skethini <110250225+skethini@users.noreply.github.com>
2024-02-18 06:21:37 -0800

    make media page

--- my-app/firebaseConfig.js
+++ my-app/firebaseConfig.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,8 +18,10 @@ const firebaseConfig = {
   measurementId: "G-T2VZ2B5BPS"
 };
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
-export { app, auth };
+export { app, auth, db };
